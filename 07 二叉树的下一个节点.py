cad63286809ac98bb57ca26f067cd7f3e49002a4
Chaojie An <anchaojie@stu.xmu.edu.cn>
2019-09-09 11:54:27 +0800

    new

--- 07 二叉树的下一个节点.py
+++ 07 二叉树的下一个节点.py
@@ -5,3 +5,26 @@ Created on Mon Sep  9 10:56:02 2019
 @author: an
 """
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#         self.father = None
+class Solution(object):
+    def inorderSuccessor(self, q):
+        """
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        cur = q
+        right = cur.right
+        if right:
+            while right:
+                pre = right
+                right = right.left
+            return pre
+        while cur.father and cur.father.left != cur:
+            cur = cur.father
+        return cur.father
