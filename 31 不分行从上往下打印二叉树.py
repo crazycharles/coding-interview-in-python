8938b49ffa9ed986e02a944173fb82de0d77f6ab
Chaojie An <anchaojie@stu.xmu.edu.cn>
2019-09-10 15:59:54 +0800

    new

---
+++ 31 不分行从上往下打印二叉树.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 15:59:29 2019
+
+@author: an
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def printFromTopToBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        # Definition for a binary tree node.
+
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        root_stack = [root]
+        leaf_stack = [root.val]
+        while root_stack:
+            temp_root = []
+            temp_leaf = []
+            for i in root_stack:
+                if i.left:
+                    temp_root.append(i.left)
+                    temp_leaf.append(i.left.val)
+                if i.right:
+                    temp_root.append(i.right)
+                    temp_leaf.append(i.right.val)
+            if temp_leaf:
+                leaf_stack.extend(temp_leaf)
+            root_stack = temp_root
+
+        return leaf_stack
+        
