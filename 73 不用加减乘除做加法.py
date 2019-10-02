29c20cbc49f54803c5c121a2de9ae5aab897d246
Chaojie An <anchaojie@stu.xmu.edu.cn>
2019-10-02 15:57:32 +0800

    Create 73 不用加减乘除做加法.py

---
+++ 73 不用加减乘除做加法.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 15:52:54 2019
+
+@author: an
+"""
+
+class Solution(object):
+    def add(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return sum([num1, num2])
